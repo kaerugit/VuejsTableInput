cf6f9615b4af170433721a79d0853da29b5ae450
kaeru <kaeru_geko2000@hotmail.co.jp>
2018-11-09 18:39:53 +0900

    ファイルアップロードサンプル追加、マスタページの追加

---
+++ javascript/masterpagecomponent.js
@@ -0,0 +1,23 @@
+﻿
+Vue.component('master-page', {
+    props:
+        {
+        }
+    ,
+    data: function () {
+        return {
+        }
+    }
+    ,
+    template: '\
+        <div class="columns">\
+            <div class="column" style="margin-top:10px">\
+                <span class="icon" onclick="window.top.location.href = \'index.html\';">\
+                    <i class="fas fa-home"></i>\
+                </span>\
+                サンプルシステム（固定ページ）\
+            </div>\
+        </div>\
+'
+});
+
